c07bab8eb2c27ce690edd0e4f6740eac4d2d8be2
starboy1101 <omkarmahabdi008@gmail.com>
2025-11-06 12:02:39 +0530

    Model changed

--- backend.py
+++ backend.py
@@ -44,8 +44,8 @@ class LlamaWrapper:
         return response["choices"][0]["text"].strip()
 
 model_path = hf_hub_download(
-    repo_id="Omkar1803/mistral-7b-gguf",
-    filename="mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+    repo_id="TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
+    filename="tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
 )
 
 llm = LlamaWrapper(
